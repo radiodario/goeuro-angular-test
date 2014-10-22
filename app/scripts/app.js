223036b717ab6077da8f30b6f9303f93a13b804d
lhache <louis.hache@goeuro.com>
2014-10-22 17:46:24 +0200

    yeoman setup

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('goeuroAngularTestApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
